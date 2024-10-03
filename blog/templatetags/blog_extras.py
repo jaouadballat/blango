764b145712fea23970d3b0742d8a12a2280dbbe2
Jaouad Ballat <jaouad.ballat@gmail.com>
2024-10-03 10:57:59 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,14 +1,31 @@
 from django.contrib.auth.models import User
 from django import template
-
+from django.utils.html import format_html
 register = template.Library()
 
 
-@register.filter(name="author_details")
-def author_details(author):
-  if not isinstance(author, User):
-    return ''
+@register.simple_tag(takes_context=True)
+def author_details(context):
+  request = context["request"]
+  current_user = request.user
+  post = context["post"]
+  author = post.author
+
+  if author == current_user:
+    return format_html("<strong>me</strong>")
+
   if author.first_name and author.last_name:
-    return f"{author.first_name}"
+    name = f"{author.first_name} {author.last_name}"
   else:
-    return f"{author.username}"
+    name = f"{author.username}"
+
+  return format_html("{}", name)
+
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html('<div class="row">', extra_classes)
+
+@register.simple_tag
+def endrow():
+    return format_html('</div>')
+
